d0a05e8dc1248470ef9cc5812e6d32fe181eeadb
nunesinc <nunes@nunesinc.net>
2022-05-31 14:47:59 -0300

    update test directory

---
+++ handbook/src/test/01_basics.ts
@@ -0,0 +1,17 @@
+//https://www.typescriptlang.org/docs/handbook/2/basic-types.html
+//hello world 
+let msg: string = "Hello World!"
+console.log(msg)
+
+
+//lower case
+const announcement = "HellO World!";
+console.log(announcement.toLocaleLowerCase())
+
+
+//greet person
+function greet(person: string, date: Date) {
+  console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+ 
+greet("Maddison", new Date());
