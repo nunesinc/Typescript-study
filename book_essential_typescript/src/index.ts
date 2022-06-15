1492c1d0a4baa2213d6caddd69df90b78c0d538a
nunesinc <nunes@nunesinc.net>
2022-06-14 22:39:13 -0300

    updated: prettier frm

--- book_essential_typescript/src/index.ts
+++ book_essential_typescript/src/index.ts
@@ -1,25 +1,6 @@
-//console.clear();
-//console.log("Nunes");
-/*
 import { TodoItem } from "./todoItem"
 import { TodoCollection } from "./todoCollection"
-let todos = [
-    new TodoItem(1, "Buy Flowers"),
-    new TodoItem(2, "Get Shoes"),
-    new TodoItem(3, "Collect Tickets"),
-    new TodoItem(4, "Call Joe", true)
-]
-let collection = new TodoCollection("Adam", todos)
-console.clear()
-console.log(`${collection.userName}'s Todo List`)
-let newId = collection.addTodo("Go for run")
-let todoItem = collection.getTodoById(newId)
-//console.log(JSON.stringify(todoItem))
-todoItem.printDetails();
-collection.addTodo(todoItem);
-*/
-import { TodoItem } from "./todoItem"
-import { TodoCollection } from "./todoCollection"
+
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"),
     new TodoItem(2, "Get Shoes"),
@@ -32,7 +13,7 @@ let collection: TodoCollection = new TodoCollection("Adam", todos)
 console.log(`${collection.userName}'s Todo List`)
 
 let newId: number = collection.addTodo("Go for run")
-let todoItem: TodoItem |undefined = collection.getTodoById(newId)
+let todoItem: TodoItem | undefined = collection.getTodoById(newId)
 todoItem?.printDetails()
 //.printDetails()
 //collection.addTodo(todoItem);
