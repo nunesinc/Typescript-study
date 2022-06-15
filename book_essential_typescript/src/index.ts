c22062eb9569b127ea028454f5ca23287a2abf3b
nunesinc <nunes@nunesinc.net>
2022-06-15 00:16:42 -0300

    added: inquirer function

--- book_essential_typescript/src/index.ts
+++ book_essential_typescript/src/index.ts
@@ -1,5 +1,6 @@
 import { TodoItem } from "./todoItem"
 import { TodoCollection } from "./todoCollection"
+import * as inquirer from 'inquirer' //lib
 
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"),
@@ -10,11 +11,30 @@ let todos: TodoItem[] = [
 
 let collection: TodoCollection = new TodoCollection("Adam", todos)
 //console.clear()
-console.log(`${collection.userName}'s Todo List` +
-`(${ collection.getItemCounts().incomplete } items to do)`
 
-)
-
+function displayTodoList(): void {
+  console.log(`${collection.userName}'s Todo List` +
+  `(${ collection.getItemCounts().incomplete } items to do)`)
+  collection.getTodoItems(true).forEach(item => item.printDetails())
+}
 
 //collection.removeComplete() //remove Itens
-collection.getTodoItems(true).forEach((item) => item.printDetails())
+//collection.getTodoItems(true).forEach((item) => item.printDetails())
+
+enum Commands {
+  Quit = "Quit"
+  }
+
+  function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+    type: "list",
+    name: "command",
+    message: "Choose option",
+    choices: Object.values(Commands)
+    }).then(answers => {if (answers["command"] !== Commands.Quit) {
+      promptUser();
+      }
+      })
+      }
